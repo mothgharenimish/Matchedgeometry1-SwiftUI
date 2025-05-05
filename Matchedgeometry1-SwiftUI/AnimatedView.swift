5ef155df9de0828cf2562876b494419778790c7f
mothgharenimish <mothgharenimish0@gmail.com>
2025-05-05 10:39:20 +0530

    Xcoded Updated Changes

--- Matchedgeometry1-SwiftUI/AnimatedView.swift
+++ Matchedgeometry1-SwiftUI/AnimatedView.swift
@@ -8,9 +8,56 @@
 import SwiftUI
 
 struct AnimatedView: View {
+    
+    @State var isExpanded: Bool = true
+      
+      // Identifier for the rectangle view
+      private var rectangleId = "Rectangle"
+      
+      // Namespace for the expansion effect
+      @Namespace var expansionAnimation
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        VStack {
+                   if isExpanded {
+                       smallSizeView()
+                   } else {
+                       largeSizeView()
+                   }
+               }
+               .padding()
+               .onTapGesture {
+                   withAnimation {
+                       isExpanded.toggle()
+                   }
+               }    }
+    
+    @ViewBuilder
+      func smallSizeView() -> some View {
+          RoundedRectangle(cornerRadius: 25)
+              .fill(.black)
+              .frame(width: 300,height: 300)
+              // Added the matched geometry modifier to the view
+              .matchedGeometryEffect(id: rectangleId, in: expansionAnimation)
+              .overlay {
+                  Text("Hello Developer")
+                      .font(.headline)
+                      .foregroundStyle(.white)
+              }
+      }
+      
+      @ViewBuilder
+      func largeSizeView() -> some View {
+          RoundedRectangle(cornerRadius: 25)
+              .fill(.black)
+              // Added the matched geometry modifier to the view
+              .matchedGeometryEffect(id: rectangleId, in: expansionAnimation)
+              .overlay {
+                  Text("Hello Developer")
+                      .font(.headline)
+                      .foregroundStyle(.white)
+              }
+      }
 }
 
 #Preview {
