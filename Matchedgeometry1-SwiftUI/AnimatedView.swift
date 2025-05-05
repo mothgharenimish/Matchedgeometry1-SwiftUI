1177ea5cd041ec8a32f62c7fd0b02c73310f772d
mothgharenimish <mothgharenimish0@gmail.com>
2025-05-05 10:38:56 +0530

    xcoded Updated Changes

---
+++ Matchedgeometry1-SwiftUI/AnimatedView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedView.swift
+//  Matchedgeometry1-SwiftUI
+//
+//  Created by Nimish Mothghare on 05/05/25.
+//
+
+import SwiftUI
+
+struct AnimatedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedView()
+}
